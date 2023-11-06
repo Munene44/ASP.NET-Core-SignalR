aa359f6db5fb146e1f67a217bbb3f409e581a7f8
Munene44 <karvinmitchar@gmail.com>
2023-11-06 11:50:09 +0300

    SendMessage method to connect client to send a message to all clients

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -1,6 +1,12 @@
-﻿namespace SignalRChat.Hubs
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
 {
-    public class ChatHub
+    public class ChatHub : Hub
     {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
     }
 }
