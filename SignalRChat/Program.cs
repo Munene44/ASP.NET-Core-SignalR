97ad8705f329253b18f41e9e1cc13b50c62d8bee
Munene44 <karvinmitchar@gmail.com>
2023-11-06 11:52:29 +0300

    signalR dependancies

--- SignalRChat/Program.cs
+++ SignalRChat/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -22,5 +23,6 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
